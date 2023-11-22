8e93087d20baa0b1683eb45614a31b0f003369e7
saraa <saraa.elhamid97@gmail.com>
2023-11-22 22:03:49 +0200

    json error

--- yolo5/app.py
+++ yolo5/app.py
@@ -6,6 +6,8 @@ import uuid
 import yaml
 from loguru import logger
 import os
+import boto3
+import pymongo
 
 images_bucket = os.environ['BUCKET_NAME']
 
@@ -26,7 +28,12 @@ def predict():
 
     # TODO download img_name from S3, store the local image path in original_img_path
     #  The bucket name should be provided as an env var BUCKET_NAME.
-    original_img_path = ...
+    original_img_path = f"{img_name}"
+    s3 = boto3.client('s3')
+    try:
+        s3.download_file(images_bucket, img_name, original_img_path)
+    except Exception:
+        logger.info(f'failed to download {img_name}')
 
     logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
 
@@ -44,9 +51,16 @@ def predict():
 
     # This is the path for the predicted image with labels
     # The predicted image typically includes bounding boxes drawn around the detected objects, along with class labels and possibly confidence scores.
-    predicted_img_path = Path(f'static/data/{prediction_id}/{original_img_path}')
 
+    predicted_img_path = Path(f'static/data/{prediction_id}/{original_img_path}')
+    predicted_img_path_str = str(predicted_img_path)
+    predicted_img = "predicted_" + img_name
     # TODO Uploads the predicted image (predicted_img_path) to S3 (be careful not to override the original image).
+    try:
+        s3.upload_file(predicted_img_path, images_bucket, predicted_img)
+    except Exception:
+        logger.info(f'failed to upload {predicted_img}')
+
 
     # Parse prediction labels and create a summary
     pred_summary_path = Path(f'static/data/{prediction_id}/labels/{original_img_path.split(".")[0]}.txt')
@@ -63,17 +77,28 @@ def predict():
             } for l in labels]
 
         logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary:\n\n{labels}')
-
         prediction_summary = {
             'prediction_id': prediction_id,
             'original_img_path': original_img_path,
-            'predicted_img_path': predicted_img_path,
+            'predicted_img_path': predicted_img_path_str,
             'labels': labels,
             'time': time.time()
         }
 
         # TODO store the prediction_summary in MongoDB
+        a_replica_set_name = "myReplicaSet"
+        hosts = ["localhost:27017", "localhost:27018", "localhost:27019", ]
+        client = pymongo.MongoClient(f"mongodb://{','.join(hosts)}/?replicaSet={a_replica_set_name}")
+        db = client["object_detection_db"]
+        collection = db["predicted_objects"]
 
+        try:
+            collection.insert_one(prediction_summary)
+        except Exception:
+            logger.info(f'failed to insert prediction for {img_name} ')
+
+        # amin:try without closing mongodb db
+        client.close()
         return prediction_summary
     else:
         return f'prediction: {prediction_id}/{original_img_path}. prediction result not found', 404
